7f456feda551b89d23f07d066f4e1e626005f43f
dalton-os <54031093+dalton-os@users.noreply.github.com>
2019-10-10 11:17:12 -0300

    adding url of source and bumping version

--- showCssAtt.user.js
+++ showCssAtt.user.js
@@ -5,11 +5,12 @@
 // ==UserScript==
 // @name         styles
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        http://*/*
 // @grant        GM_registerMenuCommand
+// @downloadurl  https://raw.githubusercontent.com/finanzero/greasemonkeys/master/showCssAtt.js
 // ==/UserScript==
 
 (function() {
